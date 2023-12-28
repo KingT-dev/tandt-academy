f6e4d38f2f4957e29f36a4b1d1c85c53da099d14
KingT-dev <154449265+KingT-dev@users.noreply.github.com>
2023-12-28 18:22:11 +0100

    database update

--- database/tandt.sql
+++ database/tandt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2023 at 01:25 PM
+-- Generation Time: Dec 28, 2023 at 06:16 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -46,6 +46,101 @@ INSERT INTO `admin` (`id`, `name`, `email`, `phone`, `password`, `status`) VALUE
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `basic1`
+--
+
+CREATE TABLE `basic1` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic2`
+--
+
+CREATE TABLE `basic2` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic3`
+--
+
+CREATE TABLE `basic3` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic4`
+--
+
+CREATE TABLE `basic4` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic5`
+--
+
+CREATE TABLE `basic5` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `classes`
 --
 
@@ -73,6 +168,177 @@ INSERT INTO `classes` (`id`, `classes`) VALUES
 (12, 'SSS 1'),
 (13, 'SSS 2'),
 (14, 'SSS 3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jss1`
+--
+
+CREATE TABLE `jss1` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jss2`
+--
+
+CREATE TABLE `jss2` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jss3`
+--
+
+CREATE TABLE `jss3` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kg1`
+--
+
+CREATE TABLE `kg1` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nursery2`
+--
+
+CREATE TABLE `nursery2` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pre_nursery`
+--
+
+CREATE TABLE `pre_nursery` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sss1`
+--
+
+CREATE TABLE `sss1` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sss2`
+--
+
+CREATE TABLE `sss2` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sss3`
+--
+
+CREATE TABLE `sss3` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -110,9 +376,93 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `basic1`
+--
+ALTER TABLE `basic1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic2`
+--
+ALTER TABLE `basic2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic3`
+--
+ALTER TABLE `basic3`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic4`
+--
+ALTER TABLE `basic4`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic5`
+--
+ALTER TABLE `basic5`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `classes`
 --
 ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jss1`
+--
+ALTER TABLE `jss1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jss2`
+--
+ALTER TABLE `jss2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jss3`
+--
+ALTER TABLE `jss3`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kg1`
+--
+ALTER TABLE `kg1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nursery2`
+--
+ALTER TABLE `nursery2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pre_nursery`
+--
+ALTER TABLE `pre_nursery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sss1`
+--
+ALTER TABLE `sss1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sss2`
+--
+ALTER TABLE `sss2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sss3`
+--
+ALTER TABLE `sss3`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -132,10 +482,94 @@ ALTER TABLE `admin`
   MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `basic1`
+--
+ALTER TABLE `basic1`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic2`
+--
+ALTER TABLE `basic2`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic3`
+--
+ALTER TABLE `basic3`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic4`
+--
+ALTER TABLE `basic4`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic5`
+--
+ALTER TABLE `basic5`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
   MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `jss1`
+--
+ALTER TABLE `jss1`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jss2`
+--
+ALTER TABLE `jss2`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jss3`
+--
+ALTER TABLE `jss3`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kg1`
+--
+ALTER TABLE `kg1`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nursery2`
+--
+ALTER TABLE `nursery2`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pre_nursery`
+--
+ALTER TABLE `pre_nursery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sss1`
+--
+ALTER TABLE `sss1`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sss2`
+--
+ALTER TABLE `sss2`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sss3`
+--
+ALTER TABLE `sss3`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `students`
